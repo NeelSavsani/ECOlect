7a602a25c4bbabfd4c501094502c1ef4a553da1a
MasterEagle <81796957+NeelSavsani@users.noreply.github.com>
2025-03-25 19:47:45 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -20,24 +20,12 @@ else
     {
         echo "Creation of Database of Failed!<br>";
     }
-    $sql = "CREATE TABLE IF NOT EXISTS `$database`.`$table` (`User_ID` INT(10) NOT NULL AUTO_INCREMENT , `Fullname` VARCHAR(30) NOT NULL , `Phone` BIGINT(10) NOT NULL , `Password` VARCHAR(20) NOT NULL , `Date-Time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`User_ID`), UNIQUE (`Phone`)) ENGINE = InnoDB;
-";
+    $sql = "CREATE TABLE IF NOT EXISTS `$database`.`$table` (`Sr.NO` INT(10) NOT NULL AUTO_INCREMENT , `Fullname` VARCHAR(30) NOT NULL, `Email` VARCHAR(30) NOT NULL , `Phone` BIGINT(10) NOT NULL , `Password` VARCHAR(20) NOT NULL , `DateTime` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`Sr.NO`)) ENGINE = InnoDB;";
     $result = mysqli_query($conn, $sql);
+    echo $result;
     if(!$result)
     {
         echo "Creation of table was failed!<br>";
-    }
-    $sql = "SELECT * FROM `$database`.`$table`;";
-    $result = mysqli_query($conn, $sql);
-    $num = mysqli_num_rows($result);
-    if($num==0)
-    {
-        $sql = "INSERT INTO `login_credentials` (`User_ID`, `Fullname`, `Phone`, `Password`, `Date-Time`) VALUES ('1', 'Neel Bipinbhai Savsani', '9712192640', 'Neel@123', current_timestamp());";
-        $result = mysqli_query($conn, $sql);
-        if(!$result)
-        {
-            echo "Insertion of data is failed". mysqli_connect_error();
-        }
     }
 }
 

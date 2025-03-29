b97173cd7ed2913d803f78fddf1e40cd0f95feed
MasterEagle <neelsavsani7@gmail.com>
2025-03-29 16:41:02 +0530

    Given path of logout.php to hyperlink

--- home.php
+++ home.php
@@ -31,7 +31,7 @@
                 <ul class="dropdown-menu" id="profile-dropdown">
                     <li><a href="#">My Profile</a></li>
                     <li><a href="#">Settings</a></li>
-                    <li><a href="#">Logout</a></li>
+                    <li><a href="login.php">Logout</a></li>
                 </ul>
             </div>
         </div>

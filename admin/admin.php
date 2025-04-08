99be8974be72f7aef1401ab00d01ef102134d1ae
MasterEagle <neelsavsani7@gmail.com>
2025-04-09 00:15:24 +0530

    Changing format of table in admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -1,55 +1,57 @@
 <?php
-echo "welcome admin";
+echo "<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Admin Panel</title>
+    <link rel='shortcut icon' href='../assets/favicon_io/favicon.ico' type='image/x-icon'>
+    <link rel='stylesheet' href='admin.css'>
+</head>
+<body>
+    <div class='container'>
+        <h1>Welcome, Admin</h1>";
+
 include '../dbconnect.php';
 $sql = "SELECT * FROM `$table`"; 
 $result = mysqli_query($conn, $sql); 
- 
-//finding the number of records returned 
+
 $nor = mysqli_num_rows($result); 
-echo "$nor"; 
-echo " Uers have registered in out portal!<br><br>"; 
-if($nor > 0) 
-{ 
-    echo "<center>"; 
-    echo "<table border=1>"; 
-    echo "<tr>"; 
-    echo "<th>Sr.No</th>"; 
-    echo "<th>Fullname</th>"; 
-    echo "<th>Email</th>"; 
-    echo "<th>Phone</th>"; 
-    echo "<th>Password</th>"; 
-    echo "<th>Address</th>"; 
-    echo "<th>Pincode</th>"; 
-    echo "<th>Date Of Registeration</th>"; 
-    echo "</tr>"; 
-    for ($i = 0; $i<$nor; $i++) 
-    { 
-        $row = mysqli_fetch_assoc($result); 
-        echo "<tr>"; 
-        echo "<td>".$row['Sr.NO']."</td>"; 
-        echo "<td>".$row['Fullname']."</td>"; 
-        echo "<td>".$row['Email']."</td>"; 
-        echo "<td>".$row['Phone']."</td>"; 
-        echo "<td>".$row['Password']."</td>"; 
-        echo "<td>".$row['Address']."</td>"; 
-        echo "<td>".$row['Pincode']."</td>"; 
-        echo "<td>".$row['DateTime']."</td>"; 
-        echo "</tr>"; 
+echo "<p><strong>$nor</strong> users have registered in our portal!</p>"; 
+
+if ($nor > 0) {
+    echo "<div class='table-wrapper'>";
+    echo "<table>";
+    echo "<tr>
+            <th>Sr.No</th>
+            <th>Fullname</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Password</th>
+            <th>Address</th>
+            <th>Pincode</th>
+            <th>Registration Date</th>
+        </tr>";
+
+    for ($i = 0; $i < $nor; $i++) {
+        $row = mysqli_fetch_assoc($result);
+        echo "<tr>
+                <td>".$row['Sr.NO']."</td>
+                <td>".$row['Fullname']."</td>
+                <td>".$row['Email']."</td>
+                <td>".$row['Phone']."</td>
+                <td>".$row['Password']."</td>
+                <td>".$row['Address']."</td>
+                <td>".$row['Pincode']."</td>
+                <td>".$row['DateTime']."</td>
+              </tr>";
     }
-    echo "</table>"; 
-    echo "</center>"; 
-} 
-?> 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Panel</title>
-    <link rel="shortcut icon" href="../assets/favicon_io/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="admin.css">
-</head>
-<body>
-    
+
+    echo "</table>";
+    echo "</div>";
+}
+
+echo "</div>
 </body>
-</html>
+</html>";
+?>
